ac0fd402d86734e02d1eddd218b1051c7112df98
kris@git <anonymousguy1000000@gmail.com>
2021-12-06 03:06:10 +0300

    chore: models storing units information

---
+++ book_units/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Units(models.Model):
+    YEAR_CHOICES=[
+    ('y1s1','First year:sem 1'),
+    ('y1s2','First year:sem 2'),
+    ('y2s1','Second year:sem 1'),
+    ('y2s2','Second year:sem 2'),
+    ('y3s1','Third year:sem 1'),
+    ('y3s2','Third year:sem 2'),
+    ('y4s1','Fourth year:sem 1'),
+    ('y4s2','Fourth year:sem 2')
+    ] 
+    year=models.CharField(default='y1s1',choices=YEAR_CHOICES,max_length=20)
+    unit_name=models.CharField(max_length=200)
+    unit_code=models.CharField(max_length=10)
+
+# Create your models here.
+
+
+class Selected_units(models.Model):
+    YEAR_CHOICES=[
+    ('y1s1','First year:sem 1'),
+    ('y1s2','First year:sem 2'),
+    ('y2s1','Second year:sem 1'),
+    ('y2s2','Second year:sem 2'),
+    ('y3s1','Third year:sem 1'),
+    ('y3s2','Third year:sem 2'),
+    ('y4s1','Fourth year:sem 1'),
+    ('y4s2','Fourth year:sem 2')
+    ] 
+    year=models.CharField(default='y1s1',choices=YEAR_CHOICES,max_length=20)
+    unit_name=models.CharField(max_length=200)
+    unit_code=models.CharField(max_length=10)
+
