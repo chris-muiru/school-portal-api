1cc12c5a424373dd22d627a43e8f4b6f06d0c0a0
kris@git <anonymousguy1000000@gmail.com>
2022-01-13 09:16:01 +0300

    add: add class based view to register new users,create token and return this token with user details.

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
 from rest_framework.authtoken.models import Token
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-        
+class register_user(APIView):
+    def post(self,request): 
+        serializer=RegisterSerializer(data=request.data)
+        data={}
+        if serializer.is_valid():
+            account = serializer.save()
+            data['response'] = "successfully registered a new user"
+            data['email'] = account.email
+            data['username'] = account.username
+            token=Token.objects.get(user=account).key
+            data['token'] = token
+        return Response(data)
 # Create your views here.
