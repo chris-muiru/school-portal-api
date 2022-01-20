e083ac8322f32c7a96aa3fd49ab6d5decfae7be8
kris@git <anonymousguy1000000@gmail.com>
2022-01-20 08:48:49 +0300

    add: add more functionality to views

--- authentication/views.py
+++ authentication/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework import  status
 
-class register_user(APIView):
+class RegisterUser(APIView):
     def post(self,request): 
         serializer=RegisterSerializer(data=request.data)
         data={}
@@ -15,5 +16,7 @@ class register_user(APIView):
             data['username'] = account.username
             token=Token.objects.get(user=account).key
             data['token'] = token
-        return Response(data)
+            data['user']=request.user
+            return Response(data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
