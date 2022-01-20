649153d006b1a73180da12f4e2a5149c94ad906d
kris@git <anonymousguy1000000@gmail.com>
2022-01-20 08:48:26 +0300

    add: create url endpoints and give them names

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from rest_framework.authtoken import views
 from django.urls import path
 urlpatterns=[
     path('register/',RegisterUser.as_view(),name="register-user"),
-    path('api-token-auth/',views.obtain_auth_token) 
+    path('api-token-auth/',views.obtain_auth_token,name="auth") 
 ]
