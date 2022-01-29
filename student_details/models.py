8dd181184b998def1bd14c2fa302355835717e4d
kris django developer <krisdjangodeveloper@gmail.com>
2022-01-29 09:51:48 +0300

    add: add new fields on model and change name of some

--- student_details/models.py
+++ student_details/models.py
@@ -4,7 +4,16 @@ from django.contrib.auth.models import User
 class UserCredentialsModel(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     school_id=models.CharField(max_length=100)
-    user_email=models.EmailField(max_length=254)
+    school_email=models.EmailField(max_length=254)
+    CHOICES=[("male","male"),("female","female"),("non-binary","non-binary")]
+    gender = models.CharField(max_length=100,choices=CHOICES)
+    country=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.user.username
 
 
 # Create your models here.
