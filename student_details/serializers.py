882036e7b277d4dacc89104e7b7b7ceb6b3b6eb0
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-01 02:57:57 +0300

    remove: remove serializer file as serializers are not required

--- student_details/serializers.py
+++ student_details/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
-from .models import UserCredentialsModel
+from .models import StudentDetailsModel
 from django.contrib.auth.models import User
 class UserCredentialsSerializer(ModelSerializer):
     class Meta:
-        model=UserCredentialsModel
+        model=StudentDetailsModel
         fields=['id','school_id','school_email','gender','country','city','phone']
