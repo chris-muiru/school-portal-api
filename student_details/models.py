467145595e7bcb54ec4d81b0f4a682aeb41fa0ca
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-01 02:56:27 +0300

    add: student details model

--- student_details/models.py
+++ student_details/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserCredentialsModel(models.Model):
+class StudentDetailsModel(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     school_id=models.CharField(max_length=100)
     school_email=models.EmailField(max_length=254)
