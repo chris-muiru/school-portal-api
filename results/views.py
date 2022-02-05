0eec9f4c4b6eb57b57075bcf811a678816d56c12
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-05 12:33:32 +0300

    add: get student results view

--- results/views.py
+++ results/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Result
 from .serializers import ResultSerializer
 from rest_framework import status
-    
+
 class Student_result(APIView):
     def get(self,request):
         queryset=Result.objects.filter(user=request.user)
