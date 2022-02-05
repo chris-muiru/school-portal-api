ade83e15c2a7cdb8c381c46eab86eb0ba0b3197f
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-05 12:26:00 +0300

    refactor: refactor model

--- results/models.py
+++ results/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from book_units.models import Selected_units
-
+from book_units.models import BookedUnit
+from django.contrib.auth.models import User
 class Result(models.Model):
-    unit=models.ForeignKey(Selected_units,on_delete=models.CASCADE,default=None)    
+    unit=models.ForeignKey(BookedUnit,on_delete=models.CASCADE,default=None)    
+    user=models.ForeignKey(User,on_delete=models.CASCADE,default=None)    
     marks=models.IntegerField()
     grade=models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.unit.unit_name}-{self.user.username}"
 
 # Create your models here.
