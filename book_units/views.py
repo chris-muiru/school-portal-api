a0f925cc6d872923996aa42fbfc92235fb028f6e
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-05 12:23:23 +0300

    refactor: use generic ciews andoverride perform_create,get_queryset methods

--- book_units/views.py
+++ book_units/views.py
@@ -1,38 +1,40 @@
+from httpcore import request
+from rest_framework.exceptions import ValidationError
 from .serializers import Get_units_serializer,Booked_units_serializer
-from .models import Selected_units,Units
+from .models import BookedUnit,Unit
 
 from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
-
+from .serializers import Booked_units_serializer
+from rest_framework import permissions
 # get all users
-class Get_Units(APIView):
-    def get(self,request):
-        queryset=Units.objects.all()
-        serializer=Get_units_serializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+from rest_framework import  generics
 
-# save selected units by user
-class Set_selected_units(APIView):
-    def post(self,request):
-        for value in request.data:
-            serializer=Booked_units_serializer(data=value)
-            if serializer.is_valid():
-                if not Selected_units.objects.filter(unit_code=serializer.validated_data['unit_code']).exists():               
-                    serializer.save()
-        return Response(request.data,status=status.HTTP_201_CREATED)
+from book_units import serializers
 
-# get all selected units by user
-class GetSelectedUnits(APIView):
-    def get(self,requet):
-        queryset=Selected_units.objects.all()
-        serializer=Booked_units_serializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+class Get_Units(generics.ListAPIView):
+    querset=Unit.objects.all()
+    serializer_class=Get_units_serializer
 
-
-
+class SetSelectedUnits(generics.CreateAPIView):
+    serializer_class=Booked_units_serializer
+    def get_queryset(self):
+        queryset=BookedUnit.objects.filter(user=self.request.user)
+        return queryset
+    def perform_create(self,serializer):
+        if BookedUnit.objects.filter(unit_code=serializer.validated_data['unit_code']).exists():
+            raise ValidationError("already selected unit")
+        serializer.save(user=self.request.user)
+   
+class GetSelectedUnits(generics.ListAPIView):
+    serializer_class=Booked_units_serializer
+    def get_queryset(self):
+        queryset=BookedUnit.objects.filter(user=self.request.user)
+        return queryset
+ 
 
 # Create your views here.
 
