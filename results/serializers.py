47553b79954dd61aecdea76322c619f46718a33c
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-05 12:25:29 +0300

    refactor: refactor seriallizer

---
+++ results/serializers.py
@@ -0,0 +1,16 @@
+from dataclasses import fields
+from numpy import source
+from rest_framework import serializers
+from .models import Result
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    user_name=serializers.ReadOnlyField(source='user.username')
+    email=serializers.ReadOnlyField(source='user.email')
+    unit_name=serializers.ReadOnlyField(source='unit.unit_name')
+    unit_code=serializers.ReadOnlyField(source='unit.unit_code')
+    class Meta:
+        model=Result
+        fields=['user_name','marks','grade','email','unit_name','unit_code']
+        ordering=['id']
+
