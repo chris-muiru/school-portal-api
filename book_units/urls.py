4747e53636cd5f0f90d14f2b8f409a7c1afbb3d1
kris django developer <krisdjangodeveloper@gmail.com>
2022-02-05 12:24:27 +0300

    rename: rename endpoints

--- book_units/urls.py
+++ book_units/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('units/',views.Get_Units.as_view()),
-    path('book/',views.Set_selected_units.as_view()),
-    path('selected/',views.GetSelectedUnits.as_view(),name='booked-units')
+    path('booked/',views.SetSelectedUnits.as_view()),
+    path('selected-units/',views.GetSelectedUnits.as_view())
 ]
 
