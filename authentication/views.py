ea8c42c1c349490402ee5953504d9e0c21ca5a30
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-02-21 03:16:33 +0300

    refactor: remove serializer. simplejwt serializer  performs functionalit

--- authentication/views.py
+++ authentication/views.py
@@ -1,22 +1,3 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializer
-from rest_framework.authtoken.models import Token
 from rest_framework import  status
-
-class RegisterUser(APIView):
-    def post(self,request): 
-        serializer=RegisterSerializer(data=request.data)
-        data={}
-        if serializer.is_valid():
-            account = serializer.save()
-            data['response'] = "successfully registered a new user"
-            data['email'] = account.email
-            data['username'] = account.username
-            token=Token.objects.get(user=account).key
-            #token= user.auth_token.key
-            data['token'] = token
-            return Response(data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-# Create your views here.
-
