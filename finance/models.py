254b83c0de7655d77e34ed159319a148e8bc3d6e
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-02-26 23:10:41 +0300

    add: add models to represent fee data

---
+++ finance/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class FeeModel(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    total_fee=models.IntegerField(default=100000)
+    paid_fee=models.IntegerField()
+    remaining_fee=models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user}-{self.paid_fee}'
+
+
+# Create your models here.
