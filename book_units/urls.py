8d3aa79f27e40c948f5e345d079c6ed2a49d6c92
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-02-26 23:09:20 +0300

    add: change endpoint name

--- book_units/urls.py
+++ book_units/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('units/',views.Get_Units.as_view()),
-    path('booked/',views.SetSelectedUnits.as_view()),
+    path('book-units/',views.SetSelectedUnits.as_view()),
     path('selected-units/',views.GetSelectedUnits.as_view())
 ]
 
