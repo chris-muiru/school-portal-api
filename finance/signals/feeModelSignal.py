1cb5410f30dd64b242a056de80f281050943e63e
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-01 15:57:59 +0300

    add: signal to calculate remaining fee

---
+++ finance/signals/feeModelSignal.py
@@ -0,0 +1,8 @@
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from ..models import FeeModel
+
+@receiver(pre_save, sender=FeeModel)
+def set_remaining_fee(sender, instance,**kwargs):
+    instance.remaining_fee = (instance.total_fee - instance.paid_fee)
