b7fc28bb3eac9e83bb3a62ac94aba3d915dca643
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-03 07:20:18 +0300

    refactor: __str__ return value

--- finance/models.py
+++ finance/models.py
@@ -8,7 +8,7 @@ class FeeModel(models.Model):
     remaining_fee=models.IntegerField(default=0)
 
     def __str__(self):
-        return f'{self.user}-{self.paid_fee}'
+        return f'{self.user}({self.paid_fee})'
 
 
 # Create your models here.
