c16b7c8cf06fdc0905362f88516484661e0f3fa6
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-09 10:02:15 +0300

    pretiffy

--- book_units/models.py
+++ book_units/models.py
@@ -1,20 +1,24 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class Unit(models.Model):
-    YEAR_CHOICES=[
-    ('y1s1','First year:sem 1'),
-    ('y1s2','First year:sem 2'),
-    ('y2s1','Second year:sem 1'),
-    ('y2s2','Second year:sem 2'),
-    ('y3s1','Third year:sem 1'),
-    ('y3s2','Third year:sem 2'),
-    ('y4s1','Fourth year:sem 1'),
-    ('y4s2','Fourth year:sem 2')
-    ] 
-    year=models.CharField(default='y1s1',choices=YEAR_CHOICES,max_length=20)
-    unit_name=models.CharField(max_length=200)
-    unit_code=models.CharField(max_length=10)
+    YEAR_CHOICES = [
+        ('y1s1', 'First year:sem 1'),
+        ('y1s2', 'First year:sem 2'),
+        ('y2s1', 'Second year:sem 1'),
+        ('y2s2', 'Second year:sem 2'),
+        ('y3s1', 'Third year:sem 1'),
+        ('y3s2', 'Third year:sem 2'),
+        ('y4s1', 'Fourth year:sem 1'),
+        ('y4s2', 'Fourth year:sem 2')
+    ]
+    year = models.CharField(
+        default='y1s1', choices=YEAR_CHOICES, max_length=20)
+    unit_name = models.CharField(max_length=200)
+    unit_code = models.CharField(max_length=10)
+
     def __str__(self):
         return self.unit_name
 
@@ -22,20 +26,21 @@ class Unit(models.Model):
 
 
 class BookedUnit(models.Model):
-    YEAR_CHOICES=[
-    ('y1s1','First year:sem 1'),
-    ('y1s2','First year:sem 2'),
-    ('y2s1','Second year:sem 1'),
-    ('y2s2','Second year:sem 2'),
-    ('y3s1','Third year:sem 1'),
-    ('y3s2','Third year:sem 2'),
-    ('y4s1','Fourth year:sem 1'),
-    ('y4s2','Fourth year:sem 2')
+    YEAR_CHOICES = [
+        ('y1s1', 'First year:sem 1'),
+        ('y1s2', 'First year:sem 2'),
+        ('y2s1', 'Second year:sem 1'),
+        ('y2s2', 'Second year:sem 2'),
+        ('y3s1', 'Third year:sem 1'),
+        ('y3s2', 'Third year:sem 2'),
+        ('y4s1', 'Fourth year:sem 1'),
+        ('y4s2', 'Fourth year:sem 2')
     ]
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    year=models.CharField(default='y1s1',choices=YEAR_CHOICES,max_length=20)
-    unit_name=models.CharField(max_length=200,default=None)
-    unit_code=models.CharField(max_length=10,default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    year = models.CharField(
+        default='y1s1', choices=YEAR_CHOICES, max_length=20)
+    unit_name = models.CharField(max_length=200, default=None)
+    unit_code = models.CharField(max_length=10, default=None)
+
     def __str__(self):
         return self.unit_name
-
